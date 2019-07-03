2a1f22b431c4c6bac880286ce5546d97b92f3136
Alexius Adhitya K <debugvelop@gmail.com>
2019-07-03 13:58:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "d1code.database.windows.net";
+    $user = "debugvelop";
+    $pass = "Dcode1Task";
+    $db = "d1code";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
